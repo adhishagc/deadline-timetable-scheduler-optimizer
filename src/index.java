1f69cf3d47dd25e8e40a6a3ffe6831bd8ae8b940
Adhisha Gammanpila <adhishagc@gmail.com>
2018-10-12 02:05:38 +0530

    Update src/index.java
    Subject inputs taken
    Subject iteration added

--- src/index.java
+++ src/index.java
@@ -11,38 +11,63 @@ public class index {
 		//Creating Scanner instance for Keyboard inputs
 		Scanner scanner = new Scanner(System.in);
 		
-		//User notice message
-		System.out.println("Please enter a date in the Exam date in the following format (MM-DD)");
+		//Subject left to ask status boolean
+		boolean anyMore = false;
 		
-		//Variable to store the user input without the year part
-		String dateInput = scanner.nextLine();
-		
-		try {
-			//Converting the user input
-			Date dateConverted = new SimpleDateFormat("yyyy-mm-dd").parse("2018-"+dateInput);
-			/*System.out.println("Converted Date "+ dateConverted.toString());*/
+		do {
+			//Get the subject input
+			System.out.println("Enter your Subject");
+			String subjectInput = scanner.nextLine();
 			
-			//Date comparison relative to today and printing outputs
-			int today = dateConverted.compareTo(new Date());
+			//User notice message
+			System.out.println("Please enter a date in the Exam date in the following format (MM-DD)");
 			
-			if(today == 0) {
+			//Variable to store the user input without the year part
+			String dateInput = scanner.nextLine();
+			
+			try {
+				//Converting the user input
+				Date dateConverted = new SimpleDateFormat("yyyy-mm-dd").parse("2018-"+dateInput);
+				/*System.out.println("Converted Date "+ dateConverted.toString());*/
 				
-				System.out.println("Date entered is Today.");
-			}
-			else if (today < 0) {
-				System.out.println("Date entered is a Past Date.");
-			}
-			else if (today > 0) {
-				System.out.println("Date entered is a Future Date.");
-			}
-			else {
-				System.out.println("Error occured");
+				//Date comparison relative to today and printing outputs
+				int today = dateConverted.compareTo(new Date());
+				
+				if(today == 0) {
+					
+					System.out.println("Date entered is Today.");
+				}
+				else if (today < 0) {
+					System.out.println("Date entered is a Past Date.");
+				}
+				else if (today > 0) {
+					System.out.println("Date entered is a Future Date.");
+				}
+				else {
+					System.out.println("Error occured");
+				}
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			
+			//Ask whether there are any more subjects
+			System.out.println("Do you have any more subjects to add, type Y for (Yes) or N for (No)");
+			String status = scanner.nextLine();
+				
+			//ToDo fix the exception situation		
+			
+			//Compare input and loop the subject collection
+			anyMore = status.equalsIgnoreCase("Y");
+			
+			
+			
+			
+		}while(anyMore);
+		
+
 
 		
 	}
